cf8081a159c04e52685f4d15ccc6a83809355927
Badr Aliloui <tranquil2021@hotmail.com>
2025-09-17 21:42:52 +0100

    Buildint the footer

--- react-project/src/components/Footer/Footer.jsx
+++ react-project/src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import './Footer.css'
 
 const Footer = () => {
+    const openingHours = 9;
+    const closingHours = 7;
+    const now = new Date();
+    const currentHour = now.getHours();
+    const currentDay = now.getDay();
+    const isWeekDay = currentDay >= 1 && currentDay <= 6;
+    const isOpen = isWeekDay && currentHour >= openingHours && currentHour < closingHours;
     return (
         <footer>
-            <p>&copy; 2024 My React App</p>
+            
+            {currentHour < openingHours || currentHour >= closingHours ? <p>We are currently closed.</p> : <p>Welcome to our website!</p>}
+            <p>{now.toString()}</p>
+
         </footer>
     );
 }
